39952646814f63cdb08a030bf86470f857264333
Rick M <rmann@latencyzero.com>
2024-01-30 18:25:16 -0800

    Initial Commit

---
+++ RepositionableViews/RepositionableViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositionableViewsApp.swift
+//  RepositionableViews
+//
+//  Created by Rick Mann on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct RepositionableViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
