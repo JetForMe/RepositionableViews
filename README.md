73ce1d7e543b8953389909ea58359952b2028ff1
Rick M <rmann@latencyzero.com>
2024-01-30 18:51:26 -0800

    First commit. WTF

---
+++ README.md
@@ -0,0 +1,5 @@
+#  Repositionable SwiftUI Views
+
+This project is an attempt to implement an elegant SwiftUI container that allows the user to
+reposition and resize the subviews contained within it using drag gestures.
+
--- RepositionableViews/ContentView.swift
+++ RepositionableViews/ContentView.swift
@@ -7,18 +7,78 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+
+
+
+
+
+struct
+Item : Identifiable
+{
+	let id									=	UUID()
+	var	name			:	String
+	var	position		:	CGPoint
+	var	size			:	CGSize
+	
+	static
+	let
+	testItems: [Item] =
+	[
+		Item(name: "Item 1", position: CGPoint(x: 150, y: 150), size: CGSize(width: 100, height: 200)),
+		Item(name: "Item 2", position: CGPoint(x: 200, y: 50), size: CGSize(width: 75, height: 100)),
+		Item(name: "Item 3", position: CGPoint(x: 100, y: 75), size: CGSize(width: 100, height: 50)),
+	]
 }
 
-#Preview {
-    ContentView()
+
+struct
+ItemView : View
+{
+	let	item			:	Item
+	
+	var
+	body: some View
+	{
+		ZStack
+		{
+			Color.green
+			Text("\(self.item.name)")
+		}
+		.frame(width: self.item.size.width, height: self.item.size.height)
+	}
+}
+
+struct
+RepositionableContainer<Content : View> : View
+{
+	let content				:	() -> Content
+	
+	var
+	body: some View
+	{
+		content()
+	}
+}
+
+struct
+ContentView: View
+{
+	var
+	body: some View
+	{
+		RepositionableContainer
+		{
+			ForEach(Item.testItems)
+			{ inItem in
+				ItemView(item: inItem)
+					.position(inItem.position)
+			}
+		}
+		.frame(width: 600, height: 400)
+	}
+}
+
+#Preview
+{
+	ContentView()
 }
